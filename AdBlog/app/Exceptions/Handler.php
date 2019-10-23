0ebc0916d754429e94a895881664b393231d01b6
digitalcrm <info@digitalcrm.com>
2019-10-24 00:54:06 +0530

    Update file

--- AdBlog/app/Exceptions/Handler.php
+++ AdBlog/app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Arr;
 
 class Handler extends ExceptionHandler
 {
@@ -63,7 +64,7 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        $guard = array_get($exception->guards(),0);
+        $guard = Arr::get($exception->guards(),0);
 
         switch ($guard) {
             case 'admin':
